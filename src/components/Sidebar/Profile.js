becd283179a2f077b683f3dcc42509b4fadf53af
Karen <devkarenc@gmail.com>
2021-05-29 21:52:24 -0400

    Make a profile component for profile pic and short introduction

---
+++ src/components/Sidebar/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import profilePic from '../../images/profilePic.png';
+import { Image, Text, Title } from '../../styles/global';
+
+const Profile = () => {
+  return (
+    <>
+      <Image
+        src={profilePic}
+        alt="Karen Choi Profile"
+        borderRadius={'50%'}
+        width={'150px'}
+      />
+      <Title>Karen Choi</Title>
+      <Text>
+        Welcome to my portfolio site! Take a minute to learn about me and my
+        journey to software engineering ☀️
+      </Text>
+    </>
+  );
+};
+
+export default Profile;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
+import Profile from './Profile';
 import Nav from './Nav';
 import SocialMediaSection from './SocialMediaSection';
-import profilePic from '../../images/profilePic.png';
-import { SidebarSection, Image, Text, Title } from '../../styles/global';
+import { SidebarSection } from '../../styles/global';
 
 const Sidebar = () => {
   return (
     <SidebarSection>
-      <Image
-        src={profilePic}
-        alt="Karen Choi Profile"
-        borderRadius={'50%'}
-        width={'150px'}
-      />
-      <Title>Karen Choi</Title>
-      <Text>
-        Welcome to my portfolio site! Take a minute to learn about me and my
-        journey to software engineering ☀️
-      </Text>
+      <Profile />
       <Nav />
       <SocialMediaSection />
     </SidebarSection>
