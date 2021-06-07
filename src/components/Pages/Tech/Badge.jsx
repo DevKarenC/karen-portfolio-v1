8dfe04668649bb9f84c83df9a844e7168c49ae9d
Karen <devkarenc@gmail.com>
2021-06-07 19:52:09 -0400

    Align text on tech stack badge text

--- src/components/Pages/Tech/Badge.jsx
+++ src/components/Pages/Tech/Badge.jsx
@@ -17,7 +17,7 @@ const Badge = ({ tech }) => {
         src={src}
         style={{ objectFit: 'scale-down', flex: '1', width: '60px' }}
       />
-      <JourneyTitle>{label}</JourneyTitle>
+      <JourneyTitle style={{ textAlign: 'center' }}>{label}</JourneyTitle>
     </Container>
   );
 };
